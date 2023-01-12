bf1e385516f326c77b900fea5b2b4ee315bccb09
Alex Cruz <lexcruthor@gmail.com>
2023-01-12 14:32:20 +0100

    segundo commit

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+
+
+export class MainPageComponent {
+  personajes: Personaje[] = [
+    {
+      nombre: ' Goku',
+      poder: 15000
+    },
+    {
+      nombre: ' Vegeta',
+      poder: 9500
+    },
+
+  ];
+  nuevo: Personaje = 
+    {
+      nombre: 'Muten Roshi',
+      poder: 1000
+    }
+  ;
+  agregarNuevoPersonaje(argumento: Personaje){
+    this.personajes.push(argumento);
+  }
+}
